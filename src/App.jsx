eb59647442c417456a9c0ed3d2c5ffcd4e146aeb
Tsotne Tskvitinidze <tskvitinidzetsotne@gmail.com>
2023-10-10 15:29:44 +0400

    firt commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import {createBrowserRouter, Route, RouterProvider, Link, BrowserRouter, Routes} from "react-router-dom";
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import HomePage from './pages/home/HomePage'
+import AboutPage from './pages/about/aboutPage'
+import VansPage from './pages/vans/vansPage';
+
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <header>
+        <Link to="/">#VanLife</Link>
+        
+        <nav>
+           <Link to="/about">About</Link>
+           <Link to="/vans">Vans</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/about' element={<AboutPage/>} />
+        <Route path='/vans' element={<VansPage/>} />
+      </Routes>
+    </BrowserRouter>
+    
+  )
+}
+
+export default App
---
+++ src/pages/vans/VansPage.jsx
@@ -0,0 +1,7 @@
+function VansPage(){
+    return (
+        <h1>Vans page goes here</h1>
+    )   
+}
+
+export default VansPage
